62ee209378b1523939b98a4d624e7000bcdbc64e
viralgupta <guptaviral0@gmail.com>
2024-07-10 11:28:20 +0530

    feat: added updateCustomer handler

--- apps/backend/src/controllers/customerController.ts
+++ apps/backend/src/controllers/customerController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import db from "@db/db";
 import { address, customer, phone_number } from "@db/schema";
-import { addAddressType, createCustomerType } from "@type/api/customer";
+import { addAddressType, createCustomerType, editCustomerType } from "@type/api/customer";
 import { eq } from "drizzle-orm";
 
 const createCustomer = async (req: Request, res: Response) => {
@@ -18,6 +18,7 @@ const createCustomer = async (req: Request, res: Response) => {
       const tCustomer = await tx.insert(customer).values({
         name: createCustomerTypeAnswer.data.name,
         balance: createCustomerTypeAnswer.data.balance,
+        profileUrl: createCustomerTypeAnswer.data.profileUrl
       }).returning();
 
       await tx.insert(phone_number).values(
@@ -57,13 +58,13 @@ const createCustomer = async (req: Request, res: Response) => {
 const addAddress = async (req: Request, res: Response) => {
   const addAddressTypeAnswer = addAddressType.safeParse(req.body);
 
-  if (!addAddressTypeAnswer.success || !req.body.customer_id){
+  if (!addAddressTypeAnswer.success){
     return res.status(400).json({success: false, message: "Input fields are not correct", error: addAddressTypeAnswer.error?.flatten()})
   }
 
   try {
-    
-    const tCustomer = await db.select({id: customer.id}).from(customer).where(eq(customer.id, req.body.customer_id));
+
+    const tCustomer = await db.select({id: customer.id}).from(customer).where(eq(customer.id, addAddressTypeAnswer.data.customer_id));
     if(tCustomer.length === 0){
       return res.status(400).json({ success: false, message: "Customer not found" });
     }
@@ -85,7 +86,38 @@ const addAddress = async (req: Request, res: Response) => {
     return res.status(500).json({success: false, message: "Unable to add address", error: error});
   }
 }
-const editCustomer = async (req: Request, res: Response) => {}
+
+const editCustomer = async (req: Request, res: Response) => {
+  const editCustomerTypeAnswer = editCustomerType.safeParse(req.body);
+
+  if (!editCustomerTypeAnswer.success){
+    return res.status(400).json({success: false, message: "Input fields are not correct", error: editCustomerTypeAnswer.error?.flatten()})
+  }
+
+  try {
+    
+    const updatedCustomer = await db.transaction(async (tx) => {
+      const tCustomer = await tx.select({id: customer.id}).from(customer).where(eq(customer.id, editCustomerTypeAnswer.data.customer_id));
+
+      if(tCustomer.length === 0){
+        throw new Error("Customer not found");
+      }
+
+      const updatedTCustomer = await tx.update(customer).set({
+        name: editCustomerTypeAnswer.data.name,
+        balance: editCustomerTypeAnswer.data.balance,
+        profileUrl: editCustomerTypeAnswer.data.profileUrl
+      }).where(eq(customer.id, tCustomer[0].id)).returning();
+
+      return updatedTCustomer[0];
+    })
+
+    return res.status(201).json({success: true, message: "Customer updated successfully", data: updatedCustomer});
+  } catch (error) {
+    return res.status(500).json({success: false, message: "Unable to update customer", error: error});
+  }
+};
+
 const settleBalance = async (req: Request, res: Response) => {}
 const getCustomer = async (req: Request, res: Response) => {}
 const deleteCustomer = async (req: Request, res: Response) => {}
--- packages/types/api/customer.ts
+++ packages/types/api/customer.ts
@@ -1,15 +1,17 @@
 import { z } from "zod";
 import { phone_numberType } from "./miscellaneous";
 
-export const addAddressType = z.object({
-  address: z.string().max(256, "Address too long"),
-  city: z.string().max(30, "City too long"),
-  state: z.string().max(20, "State too long"),
-  pincode: z.string().max(8, "Pincode too long"),
-  isPrimary: z.boolean().default(false),
-  latitude: z.number().optional(),
-  longitude: z.number().optional(),
-})
+export const addressType = z
+  .object({
+    address: z.string().max(256, "Address too long"),
+    city: z.string().max(30, "City too long"),
+    state: z.string().max(20, "State too long"),
+    pincode: z.string().max(8, "Pincode too long"),
+    isPrimary: z.boolean().default(false),
+    latitude: z.number().optional(),
+    longitude: z.number().optional(),
+  })
+  .strict("Too many fields in request body");
 
 export const createCustomerType = z
   .object({
@@ -19,12 +21,29 @@ export const createCustomerType = z
       .refine((val) => !isNaN(parseFloat(val)) && parseFloat(val) >= 0.0, {
         message: "The number must be greater than or equal to 0.00",
       }),
-    phone_numbers: z.array(
-      phone_numberType
-    ).min(1, "At least one phone number is required"),
-    addresses: z.array(
-      addAddressType
-    )
+    profileUrl: z.string().optional(),
+    phone_numbers: z
+      .array(phone_numberType)
+      .min(1, "At least one phone number is required"),
+    addresses: z.array(addressType),
   })
   .strict("Too many fields in request body");
 
+export const addAddressType =  addressType.extend({
+  customer_id: z.string()
+})
+
+export const editCustomerType = createCustomerType.omit({
+  phone_numbers: true,
+  addresses: true,
+}).extend({
+  customer_id: z.string()
+}).partial({
+  name: true,
+  balance: true,
+  profileUrl: true
+}).refine((vals) => {
+  if (!vals.balance && !vals.name && !vals.profileUrl){
+    return false;
+  }
+}, "At least one field is required to update customer")
