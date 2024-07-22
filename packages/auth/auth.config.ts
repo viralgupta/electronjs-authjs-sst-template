c2a096db4be5eeef0a0fd7ae45444d6cae09145e
viralgupta <guptaviral0@gmail.com>
2024-07-22 09:10:33 +0530

    feat: authorize user based on DB records

--- packages/auth/auth.config.ts
+++ packages/auth/auth.config.ts
@@ -1,20 +1,24 @@
 import Credentials from "@auth/express/providers/credentials";
-
+import db from "@db/db"
 const AuthConfig = {
   providers: [
     Credentials({
       credentials: {
-        phone_number: { label: "Phone Number", type: "number" },
+        phone_number: { label: "Phone Number", type: "string" },
         otp: { label: "OTP", type: "number" },
       },
       async authorize({ phone_number, otp }) {
-        console.log(phone_number, otp);
+        const user = await db.query.user.findFirst({
+          where: (user, { eq, and }) => and(eq(user.phone_number, phone_number as string), eq(user.otp, otp as number))
+        })
 
-        return {
-          id: "1234",
-          user_name: "John Doe",
-          isAdmin: false,
-        };
+        if(!user) return null;
+
+        else return {
+          id: user.id,
+          name: user.name,
+          isAdmin: user.isAdmin
+        }
       },
     }),
   ],
