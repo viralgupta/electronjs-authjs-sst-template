f37361473dcc80c1e59a78933b76c2c3d1738ed3
viralgupta <guptaviral0@gmail.com>
2024-08-02 11:20:01 +0530

    feat: initialized software with shadcn and removed unessasary files

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# TODO
+
+- Create Indexes on All Tables
+- Create Transaction Table and Create A Transaction On Each Call (Middleware)
+- convert all multiple DB queries in same request to DB Transaction
+- enable logging onto neuralic/datadog for 400 requests
+- create triggers on total_order_value for customers, carpanters, architect to change their priority
--- apps/software/electron/main/index.ts
+++ apps/software/electron/main/index.ts
@@ -6,18 +6,8 @@ import os from 'node:os'
 import { update } from './update'
 
 const require = createRequire(import.meta.url)
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.mjs   > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
 process.env.APP_ROOT = path.join(__dirname, '../..')
 
 export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
@@ -48,20 +38,14 @@ async function createWindow() {
     title: 'Main window',
     icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
     webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // nodeIntegration: true,
-
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      // contextIsolation: false,
+      preload
     },
   })
 
   if (VITE_DEV_SERVER_URL) { // #298
     win.loadURL(VITE_DEV_SERVER_URL)
     // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
   } else {
     win.loadFile(indexHtml)
   }
